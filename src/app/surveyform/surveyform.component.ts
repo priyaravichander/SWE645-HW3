828891c25df0e6fde3f5b824e16ed69c41df5b67
priyaravichander <53331445+priyaravichander@users.noreply.github.com>
2021-03-29 23:02:26 -0400

    Add files via upload

---
+++ src/app/surveyform/surveyform.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-surveyform',
+  templateUrl: './surveyform.component.html',
+  styleUrls: ['./surveyform.component.css']
+})
+export class SurveyformComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
---
+++ src/app/surveyrecords/surveyrecords.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-surveyrecords',
+  templateUrl: './surveyrecords.component.html',
+  styleUrls: ['./surveyrecords.component.css']
+})
+export class SurveyrecordsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
